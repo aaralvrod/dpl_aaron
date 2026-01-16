c54e62295a2c0deada38612507b6a807a1745f03
aaron <aaralvrod@gmail.com>
2026-01-17 00:25:28 +0100

    content added

--- ut4/a3/travelroad_django/places/views.py
+++ ut4/a3/travelroad_django/places/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 from .models import Place
 
 def index(request):
-    return render(request, 'places/index.html', {})
+    return render(request, 'place/index.html', {})
 
 def wished_places(request):
     wished = Place.objects.filter(visited=False)
-    return render(request, 'places/wished.html', {'wished': wished})
+    return render(request, 'place/wished.html', {'wished': wished})
 
 def visited_places(request):
     visited = Place.objects.filter(visited=True)
-    return render(request, 'places/visited.html', {'visited': visited})
+    return render(request, 'place/visited.html', {'visited': visited})
