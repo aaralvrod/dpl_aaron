43e8b7ce841b7224416046cc3051855671d9dca9
Aaron <aaralvrod@gmail.com>
2026-01-16 17:10:59 +0000

    content added

--- ut4/a3/travelroad_django/places/views.py
+++ ut4/a3/travelroad_django/places/views.py
@@ -5,27 +5,10 @@ from .models import Place
 
 
 def index(request):
+    wished = Place.objects.filter(visited=False)
+    visited = Place.objects.filter(visited=True)
     template = loader.get_template('places/index.html')
     context = {
-        'wished': Place.objects.filter(visited=False),
-        'visited': Place.objects.filter(visited=True),
-    }
-    return HttpResponse(template.render(context, request))
-
-
-def wished(request):
-    template = loader.get_template('places/wished.html')
-    wished = Place.objects.filter(visited=False)
-    context = {
         'wished': wished,
-    }
-    return HttpResponse(template.render(context, request))
-
-
-def visited(request):
-    template = loader.get_template('places/visited.html')
-    visited = Place.objects.filter(visited=True)
-    context = {
-        'visited': visited,
     }
     return HttpResponse(template.render(context, request))
